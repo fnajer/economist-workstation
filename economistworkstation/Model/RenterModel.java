f5e511724c0ffb996f7603b23bf453947688212b
fnajer <lexactr@gmail.com>
2018-10-31 20:04:33 +0300

    Improve RenterModel - adding renter with full set fields and deleting by id

--- economistworkstation/Model/RenterModel.java
+++ economistworkstation/Model/RenterModel.java
@@ -5,7 +5,9 @@
  */
 package economistworkstation.Model;
 
+import economistworkstation.Database;
 import economistworkstation.EconomistWorkstation;
+import economistworkstation.Entity.Renter;
 import java.sql.Statement;
 import java.sql.*;
 import java.util.logging.Level;
@@ -18,32 +20,43 @@ import java.util.ArrayList;
  */
 
 public class RenterModel {
-    public static void addRenter(Statement stmt, String name) {
+    public static void addRenter(Statement stmt, Renter renter) {
         try {
-            stmt.executeUpdate("INSERT INTO RENTER VALUES(NULL, '" + name + "')");
-            System.out.println("Добавлено: " + name);
+            //String query = "INSERT INTO RENTER VALUES(NULL, '" + renter.name + ", " + renter.surname + "')";
+            //stmt.executeUpdate(query);
+            Database db = Database.getInstance();
+            PreparedStatement ps = db.conn.prepareStatement("insert into RENTER values(NULL,?, ?, ?, ?, ?, ?)");
+            ps.setString(1,renter.name);
+            ps.setString(2,renter.surname);
+            ps.setString(3,renter.patronymic);
+            ps.setString(4,renter.address);
+            ps.setString(5,renter.birthday);
+            ps.setString(6,renter.person);
+            
+            ps.executeUpdate();
+            System.out.println("Добавлено: " + renter.name);
         } catch (SQLException ex) {
             Logger.getLogger(EconomistWorkstation.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public static void deleteRenter(Statement stmt, String name) {
+    public static void deleteRenter(Statement stmt, int id) {
         try {
-            stmt.executeUpdate("DELETE FROM RENTER WHERE name='" + name + "'");
-            System.out.println("Удалено: " + name);
+            stmt.executeUpdate("DELETE FROM RENTER WHERE id='" + id + "'");
+            System.out.println("Удалено: " + id);
         } catch (SQLException ex) {
             Logger.getLogger(EconomistWorkstation.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public static ArrayList<String> getRenters(Statement stmt) {
-        ArrayList renters = new ArrayList<String>();
+    public static ArrayList<Renter> getRenters(Statement stmt) {
+        ArrayList renters = new ArrayList<Renter>();
         try {
             ResultSet rs = stmt.executeQuery("SELECT * FROM RENTER");
             
             
             while (rs.next()) {
-                renters.add(rs.getString("name"));
+                renters.add(createObjectRenter(rs));
             }
             
             System.out.println("Извлечение арендаторов завершено.");
@@ -52,5 +65,13 @@ public class RenterModel {
             Logger.getLogger(EconomistWorkstation.class.getName()).log(Level.SEVERE, null, ex);
         }
         return renters;
+    }
+    
+    private static Renter createObjectRenter(ResultSet rs) throws SQLException {
+        Renter renter = new Renter(rs.getString("name"), rs.getString("surname"), rs.getString("patronymic"), 
+        rs.getString("address"), rs.getString("birthday"), rs.getString("person"));
+        renter.id = rs.getInt("id");
+        
+        return renter;
     }
 }
