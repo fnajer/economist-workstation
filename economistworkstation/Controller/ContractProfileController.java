f1ffcb993da8e0a8d29d0f320c90f27953516511
fnajer <lexactr@gmail.com>
2018-11-14 23:57:36 +0300

    Displaying list of months in the contract profile

--- economistworkstation/Controller/ContractProfileController.java
+++ economistworkstation/Controller/ContractProfileController.java
@@ -7,16 +7,21 @@ package economistworkstation.Controller;
 
 import economistworkstation.Entity.Building;
 import economistworkstation.Entity.Contract;
+import economistworkstation.Entity.Month;
 import economistworkstation.Entity.Renter;
 import economistworkstation.Model.BuildingModel;
 import economistworkstation.Model.ContractModel;
+import economistworkstation.Model.MonthModel;
 import economistworkstation.Model.RenterModel;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -24,6 +29,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -33,6 +40,34 @@ import javafx.scene.layout.BorderPane;
 public class ContractProfileController implements Initializable {
 
     public static int id;
+    
+    @FXML
+    private VBox containerMonths;
+
+    @FXML
+    public void showListMonths() {
+        ArrayList<Month> months = MonthModel.getMonths(id);
+
+        ObservableList listMonths = containerMonths.getChildren();  
+        listMonths.clear();
+        
+        for(Month month : months){
+            Label lblName = new Label(month.date + ' ' + month.number);
+            Button infoBtn = new Button("Подробно");
+           
+            
+            infoBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent event) {
+                    //openProfile(month.id);
+                }
+            });
+            
+            FlowPane root = new FlowPane(10, 10, lblName, infoBtn);
+            listMonths.add(root);
+        }
+    }
     /**
      * Initializes the controller class.
      */
@@ -49,12 +84,10 @@ public class ContractProfileController implements Initializable {
         
         type.setText(building.type);
         square.setText(Double.toString(building.square));
-    }   
-    
-    @FXML
-    public void showListRenters() {
         
-    }
+        showListMonths();
+    }   
+
     
     @FXML
     private Button updateBtn;
