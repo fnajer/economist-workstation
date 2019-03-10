468bbeb945129ee77f7509d0df56dcab893514b5
fnajer <lexactr@gmail.com>
2019-03-10 19:03:42 +0300

    Transfer view from MenuController in fxml

--- economistworkstation/Controller/MenuController.java
+++ economistworkstation/Controller/MenuController.java
@@ -5,26 +5,10 @@
  */
 package economistworkstation.Controller;
 
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 /**
  *
  * @author fnajer
  */
 public class MenuController {
-    public static MenuBar createMenu() {
-        MenuBar menuBar = new MenuBar();
-        
-        Menu fileMenu = new Menu("Файл");
-        Menu rentersMenu = new Menu("Арендаторы");
-        
-        MenuItem newItem = new MenuItem("Новый");
-        MenuItem exitItem = new MenuItem("Выход");
-        
-        fileMenu.getItems().addAll(newItem, exitItem);
-        menuBar.getMenus().addAll(fileMenu, rentersMenu);
-        
-        return menuBar;
-    }
+    
 }
