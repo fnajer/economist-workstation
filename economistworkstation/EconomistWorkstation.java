af5404463e0d550a4ba06d3da4dddd41c49cae8c
fnajer <lexactr@gmail.com>
2019-03-15 23:36:09 +0300

    The beginning of the restructuring app - entry point and rootLayout

--- economistworkstation/EconomistWorkstation.java
+++ economistworkstation/EconomistWorkstation.java
@@ -5,11 +5,14 @@
  */
 package economistworkstation;
 
+import economistworkstation.Controller.MenuController;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.MenuBar;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -18,25 +21,61 @@ import javafx.scene.layout.BorderPane;
  */
 public class EconomistWorkstation extends Application {
     
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    private MenuBar menu;
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        MenuBar menu = FXMLLoader.load(getClass().getResource("/economistworkstation/View/Menu.fxml"));
-        BorderPane root = FXMLLoader.load(getClass().getResource("/economistworkstation/View/MainPage.fxml"));
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Economist Workstation");
         
-        root.setTop(menu);
-        Scene scene = new Scene(root);
+        initRootLayout();
+        initMenu();
         
-        primaryStage.setTitle("Economist Workstation");
+       // showMainPage();
+    }
+    
+    public void initRootLayout() throws IOException {
+         // Загружаем корневой макет из fxml файла.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(EconomistWorkstation.class.getResource("View/RootLayout.fxml"));
+        rootLayout = (BorderPane) loader.load();
+
+        // Отображаем сцену, содержащую корневой макет.
+        Scene scene = new Scene(rootLayout);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     
-    /**
-     * @param args the command line arguments
-     */
+    public void initMenu() throws IOException {
+        FXMLLoader loaderMenu = new FXMLLoader();
+        loaderMenu.setLocation(EconomistWorkstation.class.getResource("View/Menu.fxml"));
+        menu = (MenuBar) loaderMenu.load();
+
+        rootLayout.setTop(menu); 
+        
+        MenuController controller = loaderMenu.getController();
+        controller.setRoot(rootLayout);
+        controller.setMainApp(this);
+    }
+    
+    public void showMainPage() throws IOException {
+        // Загружаем сведения об адресатах.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(EconomistWorkstation.class.getResource("View/MainPage.fxml"));
+        AnchorPane mainPage = (AnchorPane) loader.load();
+
+        // Помещаем сведения об адресатах в центр корневого макета.
+        rootLayout.setCenter(mainPage);
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
