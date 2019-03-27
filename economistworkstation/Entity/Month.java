98e573c71b85778c629fde38597461b2360236c0
fnajer <lexactr@gmail.com>
2019-03-27 22:12:39 +0300

    Month: generate monthName and calc rent

--- economistworkstation/Entity/Month.java
+++ economistworkstation/Entity/Month.java
@@ -252,6 +252,55 @@ public class Month {
         this.isPaidTaxLand.set(isPaidTaxLand);
     }
     
+    static public double calcSumRent(Month month) {
+        return month.getCost() * month.getIndexCost();
+    }
+    
+    static public String getMonthName(int monthNum) {
+        String monthName = "";
+        switch(monthNum) {
+            case 1:
+                monthName = "январь";
+                break;
+            case 2:
+                monthName = "февраль";
+                break;
+            case 3:
+                monthName = "март";
+                break;
+            case 4:
+                monthName = "апрель";
+                break;
+            case 5:
+                monthName = "май";
+                break;
+            case 6:
+                monthName = "июнь";
+                break;
+            case 7:
+                monthName = "июль";
+                break;
+            case 8:
+                monthName = "август";
+                break;
+            case 9:
+                monthName = "сентябрь";
+                break;
+            case 10:
+                monthName = "октябрь";
+                break;
+            case 11:
+                monthName = "ноябрь";
+                break;
+            case 12:
+                monthName = "декабрь";
+                break;
+            default:
+                monthName = "Not found";
+        }
+        return monthName;
+    }
+    
     @Override
     public String toString() {
         return getDate() + ' ' + getNumber();
