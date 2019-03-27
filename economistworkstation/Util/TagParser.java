d176c4f32c04e9330a986816af20b39d8681d510
fnajer <lexactr@gmail.com>
2019-03-27 22:33:40 +0300

    TagParser: add new tags and handlers for them

--- economistworkstation/Util/TagParser.java
+++ economistworkstation/Util/TagParser.java
@@ -5,11 +5,18 @@
  */
 package economistworkstation.Util;
 
+import economistworkstation.ContractData;
+import economistworkstation.Entity.Building;
+import economistworkstation.Entity.Contract;
+import economistworkstation.Entity.Month;
+import economistworkstation.Entity.Renter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 
 /**
  *
@@ -17,7 +24,12 @@ import org.apache.poi.ss.usermodel.Cell;
  */
 public class TagParser {
     
-    public static void convertTag (Cell cell) {
+    public static void convertTags (ContractData data) {
+        Cell cell = data.getCell();
+        Month month = data.getMonth();
+        Renter renter = data.getRenter();
+        Building building = data.getBuilding();
+        Contract contract = data.getContract();
         String cellString = cell.getStringCellValue();
                             
         Pattern pattern = Pattern.compile("<\\w+>");
@@ -34,8 +46,47 @@ public class TagParser {
                 String formattedString = date.format(formatter);
                 newValue = formattedString;
             }
-            if ("<numAcc>".equals(foundedTag)) {
-                newValue = "acc";
+            if ("<numRentAcc>".equals(foundedTag)) {
+                newValue = Integer.toString(month.getNumberRentAcc());
+            }
+            if ("<numCommunalAcc>".equals(foundedTag)) {
+                newValue = Integer.toString(month.getNumberCommunalAcc());
+            }
+            if ("<subject>".equals(foundedTag)) {
+                newValue = renter.getSubject();
+            }
+            if ("<fullName>".equals(foundedTag)) {
+                newValue = renter.getFullName();
+            }
+            if ("<numContract>".equals(foundedTag)) {
+                newValue = Integer.toString(contract.getId());
+            }
+            if ("<dateStartContract>".equals(foundedTag)) {
+                LocalDate date = LocalDate.parse(contract.getDateStart());
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+                String formattedString = date.format(formatter);
+                newValue = formattedString;
+            }
+            if ("<square>".equals(foundedTag)) {
+                newValue = Double.toString(building.getSquare());
+            }
+            if ("<monthName>".equals(foundedTag)) {
+                LocalDate date = LocalDate.parse(month.getDate());
+                int monthNum = date.getMonth().minus(1).getValue();
+                String monthName = Month.getMonthName(monthNum);
+                int monthYear = date.getYear();
+                if (monthNum == 12) {
+                    monthYear--;
+                }
+                newValue = monthName + ' ' + Integer.toString(monthYear);
+            }
+            if ("<sumRent>".equals(foundedTag)) {
+                String sumRent = Double.toString(Month.calcSumRent(month));
+                newValue = sumRent;
+                
+                resultString = cellString.replaceAll(foundedTag, newValue);
+                cell.setCellValue(Double.parseDouble(resultString));
+                return;
             }
             if ("<sumInWords>".equals(foundedTag)) {
                 String sumInWords = Money.digits2Text(145.00);
