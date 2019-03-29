cdfaf78a9c3067e95524f89b81a4ae8635f7639b
fnajer <lexactr@gmail.com>
2019-03-29 20:45:51 +0300

    TagParser: add tags for creating calculation account

--- economistworkstation/Util/TagParser.java
+++ economistworkstation/Util/TagParser.java
@@ -10,15 +10,16 @@ import economistworkstation.Entity.Building;
 import economistworkstation.Entity.Contract;
 import economistworkstation.Entity.Month;
 import economistworkstation.Entity.Renter;
+import economistworkstation.Entity.User;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.CellValue;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 
 /**
  *
@@ -43,6 +44,7 @@ public class TagParser {
             String foundedTag = matcher.group();
             System.out.println(foundedTag);
             
+            //rent account
             if ("<date>".equals(foundedTag)) {
                 LocalDate date = LocalDate.now();
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -76,7 +78,7 @@ public class TagParser {
             if ("<monthNameAndYear>".equals(foundedTag)) {
                 LocalDate date = LocalDate.parse(month.getDate());
                 int monthNum = date.getMonth().minus(1).getValue();
-                String monthName = Month.getMonthName(monthNum);
+                String monthName = Month.getMonthName(monthNum, false);
                 int monthYear = date.getYear();
                 if (monthNum == 12) {
                     monthYear--;
@@ -84,7 +86,7 @@ public class TagParser {
                 newValue = monthName + ' ' + Integer.toString(monthYear);
             }
             if ("<sumRent>".equals(foundedTag)) {
-                String sumRent = Double.toString(Month.calcSumRent(month));
+                String sumRent = getDecimalFormat(Locale.US).format(Month.calcSumRent(month));
                 newValue = sumRent;
                 
                 resultString = cellString.replaceAll(foundedTag, newValue);
@@ -95,7 +97,7 @@ public class TagParser {
                 return;
             }
             if ("<taxLand>".equals(foundedTag)) {
-                String taxLand = Double.toString(month.getTaxLand());
+                String taxLand = getDecimalFormat(Locale.US).format(month.getTaxLand());
                 newValue = taxLand;
                 
                 resultString = cellString.replaceAll(foundedTag, newValue);
@@ -114,7 +116,7 @@ public class TagParser {
                 String sumInWords = Money.digits2Text(sum);
                 newValue = sumInWords;
             }
-            
+            //communal account
             if ("<sumCommunalAcc>".equals(foundedTag)) {
                 cell.setCellFormula("SUM(D16:D19)");
                 return;
@@ -152,10 +154,89 @@ public class TagParser {
                 TagParser.sumForWords += resultDouble;
                 return;
             }
+             if ("<costGarbage>".equals(foundedTag)) {
+                String costGarbage = Double.toString(Month.calcCostGarbage(month));
+                newValue = costGarbage;
+                
+                resultString = cellString.replaceAll(foundedTag, newValue);
+                double resultDouble = Double.parseDouble(resultString);
+                cell.setCellValue(resultDouble);
+                
+                TagParser.sumForWords += resultDouble;
+                return;
+            }
+            // calculation account
+            if ("<sumCalcAcc>".equals(foundedTag)) {
+                cell.setCellFormula("SUM(B11:B26)");
+                return;
+            }
+            if ("<type>".equals(foundedTag)) {
+                newValue = building.getType();
+            }
+            if ("<currentMonthNameAndYear>".equals(foundedTag)) {
+                LocalDate date = LocalDate.parse(month.getDate());
+                int monthNum = date.getMonth().getValue();
+                String monthName = Month.getMonthName(monthNum, false);
+                int monthYear = date.getYear();
+
+                newValue = monthName + ' ' + Integer.toString(monthYear);
+            }
+            if ("<month>".equals(foundedTag)) {
+                LocalDate date = LocalDate.parse(month.getDate());
+                int monthNum = date.getMonth().minus(1).getValue();
+                String monthName = Month.getMonthName(monthNum, false);
+
+                newValue = monthName;
+            }
+            if ("<monthGenitive>".equals(foundedTag)) {
+                LocalDate date = LocalDate.parse(month.getDate());
+                int monthNum = date.getMonth().minus(1).getValue();
+                String monthName = Month.getMonthName(monthNum, true);
+
+                newValue = monthName;
+            }
+            if ("<getCalcSumRent>".equals(foundedTag)) {
+                String cost = getDecimalFormat(Locale.getDefault()).format(month.getCost());
+                String index = getDecimalFormat(Locale.getDefault()).format(month.getIndexCost());
+                newValue = cost + '*' + index + '=';
+            }
+            if ("<getCalcCostWater>".equals(foundedTag)) {
+                String count = getDecimalFormat(Locale.getDefault()).format(month.getCountWater());
+                String tariff = getDecimalFormat(Locale.getDefault()).format(month.getTariffWater());
+                newValue = count + '*' + tariff + '=';
+            }
+            if ("<getCalcCostElectricity>".equals(foundedTag)) {
+                String count = getDecimalFormat(Locale.getDefault()).format(month.getCountElectricity());
+                String tariff = getDecimalFormat(Locale.getDefault()).format(month.getTariffElectricity());
+                newValue = count + '*' + tariff + '=';
+            }
+            if ("<getCalcCostHeading>".equals(foundedTag)) {
+                String count = getDecimalFormat(Locale.getDefault()).format(month.getCountHeading());
+                String tariff = getDecimalFormat(Locale.getDefault()).format(month.getTariffHeading());
+                newValue = count + '*' + tariff + '=';
+            }
+            if ("<getCalcCostGarbage>".equals(foundedTag)) {
+                String count = getDecimalFormat(Locale.getDefault()).format(month.getCountGarbage());
+                String tariff = getDecimalFormat(Locale.getDefault()).format(month.getTariffGarbage());
+                newValue = count + '*' + tariff + '=';
+            }
+            if ("<user>".equals(foundedTag)) {
+                User user = new User();
+                newValue = user.getFullName();
+            }
+            
             
             resultString = cellString.replaceAll(foundedTag, newValue);
             cell.setCellValue(resultString);
             cellString = resultString;
         }
+    }
+    
+    private static DecimalFormat getDecimalFormat(Locale locale) {
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(locale);
+        DecimalFormat df = new DecimalFormat("#.##", otherSymbols);
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        
+        return df;
     }
 }
