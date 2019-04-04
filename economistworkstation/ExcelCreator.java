3036a8f19b85fa011e451a4b114678cce93b3b36
fnajer <lexactr@gmail.com>
2019-04-04 15:32:28 +0300

    ExcelCreator: method for copy rows

--- economistworkstation/ExcelCreator.java
+++ economistworkstation/ExcelCreator.java
@@ -29,6 +29,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javafx.collections.ObservableList;
 import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -39,6 +40,7 @@ import static org.apache.poi.ss.usermodel.CellType.STRING;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 /**
  *
@@ -117,8 +119,7 @@ public class ExcelCreator {
         System.out.println("Обновлен документ 'Счет для оплаты'");
     }
     
-    public static HSSFWorkbook printAccountCalculation(Contract contract, Month month) throws IOException {
-       
+    public static void printAccountCalculation(Contract contract, Month month) throws IOException {
         File file = new File("C:\\Users\\fnajer\\Desktop\\workbookCalc.xls");
 
         HSSFWorkbook workbook;
@@ -133,18 +134,13 @@ public class ExcelCreator {
             iterateCells(workbook, TagParser::convertTags, data);
         }
         
-        if ("printAll".equals(ExcelCreator.process))
-            return workbook;
-
         try (OutputStream out = new FileOutputStream("C:\\Users\\fnajer\\Desktop\\workbookCalcNew.xls")) {
             workbook.write(out);
         }
         
         System.out.println("Обновлен документ 'Расчет для оплаты'");
-        return workbook;
     }
     
-    private static String process;
     private static int indexRow;
     private static int indexColumn;
     
@@ -186,26 +182,85 @@ public class ExcelCreator {
         }
     }
     
-    public static void printCalculationAll() throws IOException {
-        OutputStream out;
-        HSSFWorkbook workbook = new HSSFWorkbook();
-                
-        Contract contract = ContractModel.getContract(51);
-        ObservableList<Month> months = MonthModel.getMonths(contract.getId());
+    
+    
+    private static void copyRow(Workbook workbook, Sheet worksheet, 
+            int sourceRowNum, int destinationRowNum) {
+        // Get the source / new row
+        Row newRow = worksheet.getRow(destinationRowNum);
+        Row sourceRow = worksheet.getRow(sourceRowNum);
 
-        ExcelCreator.process = "printAll";
-        HSSFWorkbook oneCalcWb = printAccountCalculation(contract, months.get(0));
-        rewriteWb(workbook, oneCalcWb);
+        // If the row exist in destination, push down all rows by 1 else create a new row
+        if (newRow != null) {
+            worksheet.shiftRows(destinationRowNum, worksheet.getLastRowNum(), 1, true, false);
+        } else {
+            newRow = worksheet.createRow(destinationRowNum);
+        }
 
-        out = new FileOutputStream("C:\\Users\\fnajer\\Desktop\\workbookCalcAllNew.xls");
-        workbook.write(out);
+        // Loop through source columns to add to new row
+        for (int i = 0; i < sourceRow.getLastCellNum(); i++) {
+            // Grab a copy of the old/new cell
+            Cell oldCell = sourceRow.getCell(i);
+            Cell newCell = newRow.createCell(i);
 
-//        id++;
-//        
-//        workbook = printAccountCalculation(contract, months.get(1));
-//        workbook.write(out);
-        out.close();
+            // If the old cell is null jump to next cell
+            if (oldCell == null) {
+                newCell = null;
+                continue;
+            }
 
-        System.out.println("Обновлен документ 'Общие расчеты'");
+            // Copy style from old cell and apply to new cell
+            CellStyle newCellStyle = oldCell.getCellStyle();
+            newCell.setCellStyle(newCellStyle);
+
+            // If there is a cell comment, copy
+            if (oldCell.getCellComment() != null) {
+                newCell.setCellComment(oldCell.getCellComment());
+            }
+
+            // If there is a cell hyperlink, copy
+            if (oldCell.getHyperlink() != null) {
+                newCell.setHyperlink(oldCell.getHyperlink());
+            }
+
+            // Set the cell data type
+            newCell.setCellType(oldCell.getCellType());
+
+            // Set the cell data value
+            switch (oldCell.getCellType()) {
+                case BLANK:
+                    newCell.setCellValue(oldCell.getStringCellValue());
+                    break;
+                case BOOLEAN:
+                    newCell.setCellValue(oldCell.getBooleanCellValue());
+                    break;
+                case ERROR:
+                    newCell.setCellErrorValue(oldCell.getErrorCellValue());
+                    break;
+                case FORMULA:
+                    newCell.setCellFormula(oldCell.getCellFormula());
+                    break;
+                case NUMERIC:
+                    newCell.setCellValue(oldCell.getNumericCellValue());
+                    break;
+                case STRING:
+                    newCell.setCellValue(oldCell.getRichStringCellValue());
+                    break;
+            }
+        }
+
+        // If there are are any merged regions in the source row, copy to new row
+        for (int i = 0; i < worksheet.getNumMergedRegions(); i++) {
+            CellRangeAddress cellRangeAddress = worksheet.getMergedRegion(i);
+            if (cellRangeAddress.getFirstRow() == sourceRow.getRowNum()) {
+                CellRangeAddress newCellRangeAddress = new CellRangeAddress(newRow.getRowNum(),
+                        (newRow.getRowNum() +
+                                (cellRangeAddress.getLastRow() - cellRangeAddress.getFirstRow()
+                                        )),
+                        cellRangeAddress.getFirstColumn(),
+                        cellRangeAddress.getLastColumn());
+                worksheet.addMergedRegion(newCellRangeAddress);
+            }
+        }
     }
 }
