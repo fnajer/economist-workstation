dfe4a4dbcd04a0866bbc1699a0cc4186747517dd
fnajer <lexactr@gmail.com>
2019-04-11 22:48:54 +0300

    refactor(Fine): implement abstract methods for db

--- economistworkstation/Entity/Fine.java
+++ economistworkstation/Entity/Fine.java
@@ -5,6 +5,11 @@
  */
 package economistworkstation.Entity;
 
+import economistworkstation.Database;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
@@ -30,6 +35,48 @@ public class Fine extends Payment {
     }
     public void setFine(double fine) {
         this.fine.set(fine);
+    }
+    
+    @Override
+    public int addPaymentToDb(Database db) throws SQLException {
+        int idFine = 0;
+        
+        PreparedStatement ps = db.conn.prepareStatement("insert into Fine "
+                + "(id, paid_fine, date_paid_fine, fine) "
+                + "values(NULL,?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+        ps.setDouble(1, getPaid());
+        ps.setString(2, getDatePaid());
+        ps.setDouble(3, getFine());
+
+        ResultSet rs = ps.getGeneratedKeys();
+        
+        if (rs.next()) {
+            idFine = rs.getInt(1);
+        }
+        ps.executeUpdate();
+        System.out.println("Добавлен платеж на аренду помещения: " + idFine);
+        return idFine;
+    }
+    @Override
+    public int updatePaymentToDb(Database db) throws SQLException {
+        int idFine = 0;
+        
+        PreparedStatement ps = db.conn.prepareStatement("UPDATE FINE "
+                + "SET paid_fine=?, date_paid_fine=?, fine=? "
+                + "WHERE id=?;", Statement.RETURN_GENERATED_KEYS);
+        ps.setDouble(1, getPaid());
+        ps.setString(2, getDatePaid());
+        ps.setDouble(3, getFine());
+        ps.setInt(4, getId());
+        
+        ResultSet rs = ps.getGeneratedKeys();
+        
+        if (rs.next()) {
+            idFine = rs.getInt(1);
+        }
+        ps.executeUpdate();
+        System.out.println("Изменен платеж на аренду помещения: " + idFine);
+        return idFine;
     }
     
     @Override
