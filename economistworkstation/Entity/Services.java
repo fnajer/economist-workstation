1aa316c62d22052d59a0b0c5c25d0925e878981f
fnajer <lexactr@gmail.com>
2019-04-17 13:22:17 +0300

    refactor(Services): make work with objects instead double type

--- economistworkstation/Entity/Services.java
+++ economistworkstation/Entity/Services.java
@@ -11,7 +11,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 /**
  *
@@ -19,95 +21,107 @@ import javafx.beans.property.SimpleDoubleProperty;
  */
 
 public class Services extends Payment {
-    private final DoubleProperty countWater;
-    private final DoubleProperty countElectricity;
-    private final DoubleProperty costHeading;
-    private final DoubleProperty costGarbage;
-    private final DoubleProperty costInternet;
-    private final DoubleProperty costTelephone;
-    private final DoubleProperty tariffWater;
-    private final DoubleProperty tariffElectricity;
+    private final ObjectProperty countWater;
+    private final ObjectProperty countElectricity;
+    private final ObjectProperty costHeading;
+    private final ObjectProperty costGarbage;
+    private final ObjectProperty costInternet;
+    private final ObjectProperty costTelephone;
+    private final ObjectProperty tariffWater;
+    private final ObjectProperty tariffElectricity;
     
     public Services() {
-        this(0.0, null, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+        this(0.0, null, null, null, null, null, null, null, null, null);
     }
     
-    public Services(double paid, String datePaid, double countWater, 
-            double countElectricity, double costHeading, double costGarbage,
-            double costInternet, double costTelephone, double tariffWater, 
-            double tariffElectricity) {
+    public Services(double paid, String datePaid, Object countWater, 
+            Object countElectricity, Object costHeading, Object costGarbage,
+            Object costInternet, Object costTelephone, Object tariffWater, 
+            Object tariffElectricity) {
         super(paid, datePaid);
-        this.countWater = new SimpleDoubleProperty(countWater);
-        this.countElectricity = new SimpleDoubleProperty(countElectricity);
-        this.costHeading = new SimpleDoubleProperty(costHeading);
-        this.costGarbage = new SimpleDoubleProperty(costGarbage);
-        this.costInternet = new SimpleDoubleProperty(costInternet); 
-        this.costTelephone = new SimpleDoubleProperty(costTelephone);
-        this.tariffWater = new SimpleDoubleProperty(tariffWater);
-        this.tariffElectricity = new SimpleDoubleProperty(tariffElectricity);
+        this.countWater = new SimpleObjectProperty(countWater);
+        this.countElectricity = new SimpleObjectProperty(countElectricity);
+        this.costHeading = new SimpleObjectProperty(costHeading);
+        this.costGarbage = new SimpleObjectProperty(costGarbage);
+        this.costInternet = new SimpleObjectProperty(costInternet); 
+        this.costTelephone = new SimpleObjectProperty(costTelephone);
+        this.tariffWater = new SimpleObjectProperty(tariffWater);
+        this.tariffElectricity = new SimpleObjectProperty(tariffElectricity);
     }
       
-    public double getCountWater() {
-        return countWater.get();
+    public Double getCountWater() {
+        return (Double) countWater.get();
     }
-    public void setCountWater(double countWater) {
+    public void setCountWater(Double countWater) {
         this.countWater.set(countWater);
     }
     
-    public double getCountElectricity() {
-        return countElectricity.get();
+    public Double getCountElectricity() {
+        return (Double) countElectricity.get();
     }
-    public void setCountElectricity(double countElectricity) {
+    public void setCountElectricity(Double countElectricity) {
         this.countElectricity.set(countElectricity);
     }
      
-    public double getCostHeading() {
-        return costHeading.get();
+    public Double getCostHeading() {
+        return (Double) costHeading.get();
     }
-    public void setCostHeading(double costHeading) {
+    public void setCostHeading(Double costHeading) {
         this.costHeading.set(costHeading);
     }
     
-    public double getCostGarbage() {
-        return costGarbage.get();
+    public Double getCostGarbage() {
+        return (Double) costGarbage.get();
     }
-    public void setCostGarbage(double costGarbage) {
+    public void setCostGarbage(Double costGarbage) {
         this.costGarbage.set(costGarbage);
     }
     
-     public double getCostInternet() {
-        return costInternet.get();
+     public Double getCostInternet() {
+        return (Double) costInternet.get();
     }
-    public void setCostInternet(double costInternet) {
+    public void setCostInternet(Double costInternet) {
         this.costInternet.set(costInternet);
     }
     
-     public double getCostTelephone() {
-        return costTelephone.get();
+     public Double getCostTelephone() {
+        return (Double) costTelephone.get();
     }
-    public void setCostTelephone(double costTelephone) {
+    public void setCostTelephone(Double costTelephone) {
         this.costTelephone.set(costTelephone);
     }
     
-    public double getTariffWater() {
-        return tariffWater.get();
+    public Double getTariffWater() {
+        return (Double) tariffWater.get();
     }
-    public void setTariffWater(double tariffWater) {
+    public void setTariffWater(Double tariffWater) {
         this.tariffWater.set(tariffWater);
     }
     
-    public double getTariffElectricity() {
-        return tariffElectricity.get();
+    public Double getTariffElectricity() {
+        return (Double) tariffElectricity.get();
     }
-    public void setTariffElectricity(double tariffElectricity) {
+    public void setTariffElectricity(Double tariffElectricity) {
         this.tariffElectricity.set(tariffElectricity);
     }
     
-    public double calcCostWater(Services services) {
-        return services.getCountWater() * services.getTariffWater();
+    public Double calcCostWater() {
+        return getCountWater() * getTariffWater();
     }
-    public double calcCostElectricity(Services services) {
-        return services.getCountElectricity() * services.getTariffElectricity();
+    public Double calcCostElectricity() {
+        return getCountElectricity() * getTariffElectricity();
+    }
+    public Double calcCostHeading() {
+        return getCostHeading();
+    }
+    public Double calcCostGarbage() {
+        return getCostGarbage();
+    }
+    public Double calcCostInternet() {
+        return getCostInternet();
+    }
+    public Double calcCostTelephone() {
+        return getCostTelephone();
     }
     
     @Override
@@ -119,33 +133,35 @@ public class Services extends Payment {
                 + "VALUES(NULL,?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
         ps.setDouble(1, getPaid());
         ps.setString(2, getDatePaid());
-        ps.setDouble(3, getCountWater());
-        ps.setDouble(4, getTariffWater());
-        ps.setDouble(5, getCountElectricity());
-        ps.setDouble(6, getCostHeading());
-        ps.setDouble(7, getCostGarbage());
-        ps.setDouble(8, getCostInternet());
-        ps.setDouble(9, getCostTelephone());
+        ps.setObject(3, getCountWater(), java.sql.Types.DOUBLE);
+        ps.setObject(4, getTariffWater(), java.sql.Types.DOUBLE);
+        ps.setObject(5, getCountElectricity(), java.sql.Types.DOUBLE);
+        ps.setObject(6, getTariffElectricity(), java.sql.Types.DOUBLE);
+        ps.setObject(7, getCostHeading(), java.sql.Types.DOUBLE);
+        ps.setObject(8, getCostGarbage(), java.sql.Types.DOUBLE);
+        ps.setObject(9, getCostInternet(), java.sql.Types.DOUBLE);
+        ps.setObject(10, getCostTelephone(), java.sql.Types.DOUBLE);
         
         return ps;
     }
     @Override
     public PreparedStatement getUpdateStatement(Database db) throws SQLException {
         PreparedStatement ps = db.conn.prepareStatement("UPDATE SERVICES "
-                + "SET paid_services=?, date_paid_services=?, count_water=?, tariff_water=? "
+                + "SET paid_services=?, date_paid_services=?, count_water=?, tariff_water=?, "
                 + "count_electricity=?, tariff_electricity=?, cost_meter_heading=?, "
                 + "cost_meter_garbage=?, cost_internet=?, cost_telephone=? "
                 + "WHERE id=?", Statement.RETURN_GENERATED_KEYS);
         ps.setDouble(1, getPaid());
         ps.setString(2, getDatePaid());
-        ps.setDouble(3, getCountWater());
-        ps.setDouble(4, getTariffWater());
-        ps.setDouble(5, getCountElectricity());
-        ps.setDouble(6, getCostHeading());
-        ps.setDouble(7, getCostGarbage());
-        ps.setDouble(8, getCostInternet());
-        ps.setDouble(9, getCostTelephone());
-        ps.setInt(10, getId());
+        ps.setObject(3, getCountWater(), java.sql.Types.DOUBLE);
+        ps.setObject(4, getTariffWater(), java.sql.Types.DOUBLE);
+        ps.setObject(5, getCountElectricity(), java.sql.Types.DOUBLE);
+        ps.setObject(6, getTariffElectricity(), java.sql.Types.DOUBLE);
+        ps.setObject(7, getCostHeading(), java.sql.Types.DOUBLE);
+        ps.setObject(8, getCostGarbage(), java.sql.Types.DOUBLE);
+        ps.setObject(9, getCostInternet(), java.sql.Types.DOUBLE);
+        ps.setObject(10, getCostTelephone(), java.sql.Types.DOUBLE);
+        ps.setInt(11, getId());
         
         return ps;
     }
