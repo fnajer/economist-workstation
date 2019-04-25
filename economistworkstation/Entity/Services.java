6e8fb105f440e9c177d54f8826df21e616a246f1
fnajer <lexactr@gmail.com>
2019-04-25 21:15:51 +0300

    feat(Services): override method to receive sum of services

--- economistworkstation/Entity/Services.java
+++ economistworkstation/Entity/Services.java
@@ -18,14 +18,14 @@ import javafx.beans.property.SimpleObjectProperty;
  */
 
 public class Services extends Payment {
-    private final ObjectProperty countWater;
-    private final ObjectProperty countElectricity;
-    private final ObjectProperty costHeading;
-    private final ObjectProperty costGarbage;
-    private final ObjectProperty costInternet;
-    private final ObjectProperty costTelephone;
-    private final ObjectProperty tariffWater;
-    private final ObjectProperty tariffElectricity;
+    private final ObjectProperty<Double> countWater;
+    private final ObjectProperty<Double> countElectricity;
+    private final ObjectProperty<Double> costHeading;
+    private final ObjectProperty<Double> costGarbage;
+    private final ObjectProperty<Double> costInternet;
+    private final ObjectProperty<Double> costTelephone;
+    private final ObjectProperty<Double> tariffWater;
+    private final ObjectProperty<Double> tariffElectricity;
     
     public Services() {
         this(null, null, null, null, null, null, null, null, null, null, null);
@@ -47,102 +47,78 @@ public class Services extends Payment {
     }
       
     public Double getCountWater() {
-        return (Double) countWater.get();
+        return countWater.get();
     }
     public void setCountWater(Double countWater) {
         this.countWater.set(countWater);
     }
     
     public Double getCountElectricity() {
-        return (Double) countElectricity.get();
+        return countElectricity.get();
     }
     public void setCountElectricity(Double countElectricity) {
         this.countElectricity.set(countElectricity);
     }
      
     public Double getCostHeading() {
-        return (Double) costHeading.get();
+        return costHeading.get();
     }
     public void setCostHeading(Double costHeading) {
         this.costHeading.set(costHeading);
     }
     
     public Double getCostGarbage() {
-        return (Double) costGarbage.get();
+        return costGarbage.get();
     }
     public void setCostGarbage(Double costGarbage) {
         this.costGarbage.set(costGarbage);
     }
     
      public Double getCostInternet() {
-        return (Double) costInternet.get();
+        return costInternet.get();
     }
     public void setCostInternet(Double costInternet) {
         this.costInternet.set(costInternet);
     }
     
      public Double getCostTelephone() {
-        return (Double) costTelephone.get();
+        return costTelephone.get();
     }
     public void setCostTelephone(Double costTelephone) {
         this.costTelephone.set(costTelephone);
     }
     
     public Double getTariffWater() {
-        return (Double) tariffWater.get();
+        return tariffWater.get();
     }
     public void setTariffWater(Double tariffWater) {
         this.tariffWater.set(tariffWater);
     }
     
     public Double getTariffElectricity() {
-        return (Double) tariffElectricity.get();
+        return tariffElectricity.get();
     }
     public void setTariffElectricity(Double tariffElectricity) {
         this.tariffElectricity.set(tariffElectricity);
     }
     
     public Double calcCostWater() {
-        try {
-            return getCountWater() * getTariffWater();
-        } catch(NullPointerException e) {
-            return 0.0;
-        }
+        return safeGetSum(getCountWater(), getTariffWater());
     }
     public Double calcCostElectricity() {
-        try {
-            return getCountElectricity() * getTariffElectricity();
-        } catch(NullPointerException e) {
-            return 0.0;
-        }
+        return safeGetSum(getCountElectricity(), getTariffElectricity());
     }
     public Double calcCostHeading() {
-        Double cost = getCostHeading();
-        if (cost != null)
-            return cost;
-        else
-            return 0.0;
+        return safeGetSum(getCostHeading());
     }
     public Double calcCostGarbage() {
-        Double cost = getCostGarbage();
-        if (cost != null)
-            return cost;
-        else
-            return 0.0;
+        return safeGetSum(getCostGarbage());
     }
     public Double calcCostInternet() {
-        Double cost = getCostInternet();
-        if (cost != null)
-            return cost;
-        else
-            return 0.0;
+        return safeGetSum(getCostInternet());
     }
     public Double calcCostTelephone() {
-        Double cost = getCostTelephone();
-        if (cost != null)
-            return cost;
-        else
-            return 0.0;
+        return safeGetSum(getCostTelephone());
     }
     
     @Override
@@ -153,16 +129,16 @@ public class Services extends Payment {
                 + "cost_meter_garbage, cost_internet, cost_telephone) "
                 + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
         ps.setInt(1, getId());
-        ps.setObject(2, getPaid(), java.sql.Types.DOUBLE);
+        ps.setObject(2, getPaid());
         ps.setString(3, getDatePaid());
-        ps.setObject(4, getCountWater(), java.sql.Types.DOUBLE);
-        ps.setObject(5, getTariffWater(), java.sql.Types.DOUBLE);
-        ps.setObject(6, getCountElectricity(), java.sql.Types.DOUBLE);
-        ps.setObject(7, getTariffElectricity(), java.sql.Types.DOUBLE);
-        ps.setObject(8, getCostHeading(), java.sql.Types.DOUBLE);
-        ps.setObject(9, getCostGarbage(), java.sql.Types.DOUBLE);
-        ps.setObject(10, getCostInternet(), java.sql.Types.DOUBLE);
-        ps.setObject(11, getCostTelephone(), java.sql.Types.DOUBLE);
+        ps.setObject(4, getCountWater());
+        ps.setObject(5, getTariffWater());
+        ps.setObject(6, getCountElectricity());
+        ps.setObject(7, getTariffElectricity());
+        ps.setObject(8, getCostHeading());
+        ps.setObject(9, getCostGarbage());
+        ps.setObject(10, getCostInternet());
+        ps.setObject(11, getCostTelephone());
         
         return ps;
     }
@@ -173,16 +149,16 @@ public class Services extends Payment {
                 + "count_electricity=?, tariff_electricity=?, cost_meter_heading=?, "
                 + "cost_meter_garbage=?, cost_internet=?, cost_telephone=? "
                 + "WHERE id_services=?", Statement.RETURN_GENERATED_KEYS);
-        ps.setObject(1, getPaid(), java.sql.Types.DOUBLE);
+        ps.setObject(1, getPaid());
         ps.setString(2, getDatePaid());
-        ps.setObject(3, getCountWater(), java.sql.Types.DOUBLE);
-        ps.setObject(4, getTariffWater(), java.sql.Types.DOUBLE);
-        ps.setObject(5, getCountElectricity(), java.sql.Types.DOUBLE);
-        ps.setObject(6, getTariffElectricity(), java.sql.Types.DOUBLE);
-        ps.setObject(7, getCostHeading(), java.sql.Types.DOUBLE);
-        ps.setObject(8, getCostGarbage(), java.sql.Types.DOUBLE);
-        ps.setObject(9, getCostInternet(), java.sql.Types.DOUBLE);
-        ps.setObject(10, getCostTelephone(), java.sql.Types.DOUBLE);
+        ps.setObject(3, getCountWater());
+        ps.setObject(4, getTariffWater());
+        ps.setObject(5, getCountElectricity());
+        ps.setObject(6, getTariffElectricity());
+        ps.setObject(7, getCostHeading());
+        ps.setObject(8, getCostGarbage());
+        ps.setObject(9, getCostInternet());
+        ps.setObject(10, getCostTelephone());
         ps.setInt(11, getId());
         
         return ps;
@@ -190,10 +166,32 @@ public class Services extends Payment {
     @Override
     public PreparedStatement getDeleteStatement(Database db) throws SQLException {
         PreparedStatement ps = db.conn.prepareStatement("DELETE FROM SERVICES "
-                + "WHERE id_services=?", Statement.RETURN_GENERATED_KEYS);
+                + "WHERE id_services=?");
         ps.setInt(1, getId());
         
         return ps;
+    }
+    
+    @Override
+    public Double sumToPay() {
+        return sum(
+            calcCostWater(),
+            calcCostElectricity(),
+            calcCostHeading(),
+            calcCostGarbage(),
+            calcCostInternet(),
+            calcCostTelephone()
+        );
+    }
+    
+    private Double sum(Double ...values) {
+        Double sum = 0.0;
+        for (Double value : values) {
+            try {
+                sum += value;
+            } catch(NullPointerException e) {}
+        }
+        return sum; 
     }
    
     @Override
