933237bf5d23a0efd283370966716974976cffb8
fnajer <lexactr@gmail.com>
2019-04-26 23:18:07 +0300

    fix(Payment): catch exceptions for diff getter

--- economistworkstation/Entity/Payment.java
+++ economistworkstation/Entity/Payment.java
@@ -6,12 +6,11 @@
 package economistworkstation.Entity;
 
 import economistworkstation.Database;
+import static economistworkstation.Util.Util.isExist;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -43,6 +42,8 @@ public abstract class Payment {
     public abstract PreparedStatement getDeleteStatement(Database db) throws SQLException;
     
     public abstract Double sumToPay();
+    public abstract boolean isEmpty();
+//    public abstract void fill();
     
     public int getId() {
         return id.get();
@@ -75,9 +76,17 @@ public abstract class Payment {
     }
     
     public Double getDiff() {
-        Double needPay = sumToPay();
-        Double diff = needPay - getPaid();
-        return diff;
+        try {
+            Double needPay = sumToPay();
+            Double diff = needPay - safeGetSum(getPaid());
+            return diff;
+        } catch(NullPointerException e) {
+            System.out.println(String.format(
+                    "%s: %s diff is null",
+                    this.getClass().getSimpleName(),
+                    this));
+            return 0.0;
+        }
     }
     
     protected Double safeGetSum(Double value) {
