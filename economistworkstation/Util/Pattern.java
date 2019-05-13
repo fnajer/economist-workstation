b8786fed230590edf926bcc50e036a5643edd7a1
fnajer <lexactr@gmail.com>
2019-05-13 21:40:54 +0300

    refactor(Pattern): extend Statement class to Pattern class
    To the successor not only statements, but also bills of payments

---
+++ economistworkstation/Util/Pattern.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package economistworkstation.Util;
+
+import economistworkstation.ContractDataParameters;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author fnajer
+ */
+public abstract class Pattern {
+    private String pathToSave;
+    private String patternPath;
+    private final String patternDirPath = "src/config/patterns";        
+    public Pattern() {
+        
+    }
+    
+    protected String constructPatternPath(String fileName) {
+        return Paths.get(patternDirPath).resolve(fileName).toString();
+    }
+    
+    
+    public String getPathToSave() {
+        return pathToSave;
+    }
+    public void setPathToSave(String pathToSave) {
+        this.pathToSave = pathToSave;
+    }
+    
+    public String getPatternPath() {
+        return patternPath;
+    }
+    public void setPatternPath(String patternPath) {
+        this.patternPath = constructPatternPath(patternPath);
+    }
+    
+    public abstract void print(ContractDataParameters data);
+}
